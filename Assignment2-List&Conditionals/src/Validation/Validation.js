5f59712f7be516e019154bc4ccf6d88c573a1c16
Nidhi <nidhisharmas0103@gmail.com>
2020-07-16 15:49:34 -0400

    created components + functionality works

---
+++ Assignment2-List&Conditionals/src/Validation/Validation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const validation = (props) => {
+    let validationMessage = 'Text is long enough';
+
+    if(props.inputLength <= 5){
+        validationMessage = 'Text is too short!'
+    }
+    return (
+        <div>
+            <p>{validationMessage}</p>
+        </div>
+    )
+}
+
+export default validation;
