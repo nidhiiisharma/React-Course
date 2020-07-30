38b0afb4eb38ebceaef7cc7201c5ea1d36a7dd49
Nidhi <nidhisharmas0103@gmail.com>
2020-07-30 16:55:15 -0400

    splitting app into components

--- Section7-ComponentDive/src/containers/App.js
+++ Section7-ComponentDive/src/containers/App.js
@@ -1,25 +1,7 @@
 import React, { Component } from 'react';
-/* import Radium, { StyleRoot } from 'radium'; // 3rd party package for psuedo selectors & media queries */
-/* import styled from 'styled-components'; // 3rd party package for styling  */
 import classes from './App.css';
-import Person from '../components/PersonList/Person/Person';
-
-/* const StyledButton = styled.button`
-  background-color: ${props => props.alt ? 'blue' : 'green'};
-  color: white; 
-  font: inherit;
-  border: ${props => props.alt ? '1px solid blue' : '1px solid green'};
-  border-radius: 4px;
-  padding: 8px;
-  cursor: pointer;
-  outline: none;
-  
-  &:hover {
-    background-color: ${props => props.alt ? 'skyblue' : 'lightgreen'};
-    border: ${props => props.alt ? '1px solid skyblue' : '1px solid lightgreen'}; 
-    color: black;
-  }
-`; */
+import PersonList from '../components/PersonList/PersonList';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
   state = {
@@ -61,75 +43,27 @@ class App extends Component {
   }
 
   render() {
-    /* const style = {
-      backgroundColor: 'green',
-      color: 'white', 
-      font: 'inherit',
-      border: '1px solid green',
-      borderRadius: '4px',
-      padding: '8px',
-      cursor: 'pointer',
-      outline: 'none',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        border: '1px solid lightgreen',
-        color: 'black'
-      }
-    } */
 
     let persons = null;
-    let btnClass = '';
 
     if( this.state.showPersons ){
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <Person 
-              click={() => this.deletePersonHandler(index)}
-              name={person.name}
-              age={person.age}
-              key={person.id} 
-              changed={(event) => this.nameChangedHandler(event, person.id)}/>
-          })}
-        </div>
-      )
-
-      btnClass = classes.blue;
-
-  /*  style.backgroundColor = 'blue';
-      style.border = '1px solid blue';
-
-      style[':hover'] = {
-        backgroundColor: 'skyblue',
-        border: '1px solid skyblue',
-        color: 'black'
-      } */
-    }
-
-    const assignedClasses = [];
-    if (this.state.persons.length <= 2){
-      assignedClasses.push(classes.red); // classes = ['red']
-    }
-    if (this.state.persons.length <= 1){
-      assignedClasses.push(classes.bold) // classes = ['red','bold']
+      persons = <PersonList
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangedHandler} />;
     }
 
     return (
-      // use styleroot so media queries can work
-      // <StyleRoot >
         <div className={classes.App}>
-        <h1>This is my first react app</h1>
-        <p className={assignedClasses.join(' ')}>This is really working!</p>
-        <button className={btnClass} alt={this.state.showPersons} onClick={this.togglePersonsHandler}>
-          Toggle Persons
-        </button>
+          <Cockpit 
+            showPersons={this.state.showPersons}
+            persons={this.state.persons}
+            clicked={this.togglePersonsHandler}
+          />
         {persons}
       </div>
-      // </StyleRoot>
     );
-    //return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Does this work now?'))
     }
   }
 
-  // export default Radium(App);
   export default App;
